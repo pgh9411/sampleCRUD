1873e6edc5ba9634354553538a247de1e1ab3b61
gnsl8 <gnsl8@221.139.195.122>
2019-02-13 19:28:07 +0900

    [박기훈]SampleService,SampleController 1.addSample(only폼) 2.addSample(액션) 3.sampleList(db데이터 나오게) 4.removeSample 5.modifySample(수정폼) 6.modifySample(수정 액션) 1.입력폼  2.입력 액션  3.목록  4.삭제 액션  5.수정 폼  6.수정 액션

--- web03/src/main/java/cafe/ebs/web03/controller/SampleController.java
+++ web03/src/main/java/cafe/ebs/web03/controller/SampleController.java
@@ -17,24 +17,33 @@ public class SampleController {
 	@Autowired private SampleService sampleService;
 	
 	
+	
 	// 1. 입력폼
 	@GetMapping(value="/addSample")
+	//get으로 addSample을 들어오면 입력폼을 출력한다
+	//Get이기 때문에 GetMapping
 	public String addSample() {
 		System.out.println("get mapping addSample");
 		System.out.println("입력화면 출력");
 		return "addSample"; // view이름은 templates폴더에 addSample.html 
 	}
-	//Get이기 때문에 GetMapping
+	
 	// 2. 입력 액션
 	@PostMapping("/addSample")
+	//입력은 post로 들어와서 입력액션을 한다
+	//Post이기 때문에 PostMapping
 	public String addSample(@RequestParam(value="sampleName") String sampleName) {
+		//어노테이션을 RequestParam sampleName 객체에 입력받은 sampleName값을 저장한다
 		System.out.println("post mapping addSample");
 		sampleService.addSample(sampleName);
+		//
 		System.out.println("추가한 NAME : " + sampleName);
 		return "redirect:/sampleList";
+		//입력 후 sampleList로 경로 요청한다
 	}
-	//Post이기 때문에 PostMapping,
+	
 	//첫번째.뒤의 sampleName을 먼저 만들고 초기 null값
+	
 	//두번째.@RequestParam이용해 request.getParameter로  sampleName을 채운다
 	
 	// 3. 목록
@@ -43,11 +52,13 @@ public class SampleController {
 	public String sampleList(Model model) {
 		System.out.println("get mapping sampleList");
 		List<Sample> list = sampleService.getSampleList();
-
+		//List<Sample>타입 list 객체에 getSampleList메서드 호출하여 리턴값을 넣는다 
 		model.addAttribute("list", list);
+		//model 이용해서 "list"에 세팅하는 역할
 		System.out.println("리스트 출력");
 		return "sampleList";
 	}
+	
 	//request.setAttribute는 model 사용
 	// 4. 삭제 액션
 	@GetMapping("/removeSample")
@@ -77,12 +88,16 @@ public class SampleController {
 	}
 	// 6. 수정 액션
 	@PostMapping("/modifySample")
+	//post로 /modifySample 을 들어오면 수정 액션을 한다
 	public String modifySample(@RequestParam(value="sampleName")String sampleName,@RequestParam(value="sampleId")int sampleId) {
+		//어노테이션 RequestParam 을 이용해 sampleName에 sampleName 입력값을 넣고, sampleId객체에 sampleId 입력값을 넣는다
 		System.out.println("post mapping modifySample");
-		int result = sampleService.modifySample(sampleId,sampleName);
+		sampleService.modifySample(sampleId,sampleName);
+		//modifySample메서드에 입력받은 sampleId와 sampleName을 넣는다
 		System.out.println("ID : "+sampleId);
 		System.out.println("수정한 NAME : "+sampleName);
 		return "redirect:/sampleList";
+		//수정 후 sampleList로 경로 요청한다.
 	}
 
 }
--- web03/src/main/java/cafe/ebs/web03/service/SampleService.java
+++ web03/src/main/java/cafe/ebs/web03/service/SampleService.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import cafe.ebs.web03.mapper.SampleMapper;
 import cafe.ebs.web03.vo.Sample;
 
@@ -13,30 +11,37 @@ import cafe.ebs.web03.vo.Sample;
 public class SampleService {
 	@Autowired
 	private SampleMapper sampleMapper;
-	// 애노테이션 사이에 주석 넣지 않기
+	// 애노테이션 사이에 주석 넣지 않기.
 
+	
 	public List<Sample> getSampleList() {
 		List<Sample> list = sampleMapper.selectSampleList();
-		
+		//List<Sample> list 안에 selectSampleList 메서드 호출한 리턴값을 넣는다
+		//호출 한 값들이 들어 있는  list를 리턴한다
 		return list;
 	}
 
 	public List<Sample> getSampleOne(int sampleId) {
 		List<Sample> list = sampleMapper.selectSampleOne(sampleId);
-		
+		//List<Sample> list 안에 selectSampleOne 메서드에 sampleId 입력 후  호출한 리턴값을 넣는다
+		//호출 한 값들이 들어 있는  list를 리턴한다
 		return list;
 	}
 
 	public int addSample(String sampleName) {
 		int result = sampleMapper.insertSample(sampleName);
-		
+		//insertSample메서드에 sampleName을 입력값으로 하여 메서드 호출한다
+		//id를 자동 증가하고 name을 리스트에 추가한다
+		//리턴값을 result객체에 넣어 메서드 실행되는지 확인할 수 있다
 		//return 유무?
 		return result;
 	}
 
 	public int removeSample(int sampleId) {
 		int result = sampleMapper.deleteSample(sampleId);
-		
+		//deleteSample메서드에 sampleId을 입력값으로 하여 메서드 호출한다
+		//삭제할 id를 가져와 삭제시킨다
+		//리턴값을 result객체에 넣어 메서드 실행되는지 확인할 수 있다
 		//return 유무?
 		return result;
 	}
@@ -44,8 +49,10 @@ public class SampleService {
 	public int modifySample(int sampleId, String sampleName) {
 		//sampleId와 sampleName을 입력값으로 받아 수정액션을 한다
 		int result = sampleMapper.updateSample(sampleId,sampleName);
-		
+		//updateSample메서드에 수정 폼에서 입력받은  sampleId, sampleName을 이용하여 수정 한다 
+		//리턴값을 result객체에 넣어 메서드 실행되는지 확인할 수 있다
+
 		return result;
-	}// 리턴값은 나중에 수정하기 o
+	}// 리턴값은 나중에 수정하기 완료
 
 }
